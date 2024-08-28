dc1c56dd9bb5e3843deed5533e7edf7efd9ed60f
Harshad Gangurde <143880663+codeitharshad@users.noreply.github.com>
2024-08-28 12:34:48 +0530

    Update airbnb badge

--- airbnb/src/components/Card.jsx
+++ airbnb/src/components/Card.jsx
@@ -1,25 +1,33 @@
-import '../index.css'
-import star from '../assets/star.png'
+import "../index.css";
+import star from "../assets/star.png";
 
+export default function (props) {
+  
+  let badgeText;
 
+  if(props.openSpots === 0){
+    badgeText = "SOLD OUT";
+  }else if(props.location ==="online"){
+    badgeText = "ONLINE";
+  }
 
-export default function({img, rating, reviewCount, country, title, price}){
-    return (
-      <div className="card">
-        <img src={img} className="card--image" alt="Image" />
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={props.coverImg} className="card--image" alt="Image" />
 
-        <div className="card--stats">
-          <img src={star} alt="star" className="card--star" />
+      <div className="card--stats">
+        <img src={star} alt="star" className="card--star" />
 
-          <span className="card--rating">{rating}</span>
-          <span className="gray">({reviewCount}) • </span>
-          <span className="gray"> {country}</span>
-        </div>
-
-        <h2 className="card--tagline">{title}</h2>
-        <p>
-          <span className="bold">From ${price}</span> / person
-        </p>
+        <span className="card--rating">{props.stats.rating}</span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
+        <span className="gray"> {props.location}</span>
       </div>
-    );
+
+      <h2 className="card--tagline">{props.title}</h2>
+      <p>
+        <span className="bold">From ${props.price}</span> / person
+      </p>
+    </div>
+  );
 }
--- airbnb/src/data.js
+++ airbnb/src/data.js
@@ -1,7 +1,7 @@
 export default [
   {
     id: 1,
-    title: "Life Lessons with Katie Zaferes",
+    title: "Life Lessons with Katie ",
     description:
       'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
     price: 136,
@@ -10,7 +10,7 @@ export default [
       rating: 5.0,
       reviewCount: 6,
     },
-    location: "Nashik",
+    location: "online",
     openSpots: 0,
   },
   {
@@ -24,7 +24,7 @@ export default [
       rating: 5.0,
       reviewCount: 30,
     },
-    location: "Pimpalgaon",
+    location: "online",
     openSpots: 27,
   },
   {
@@ -42,8 +42,8 @@ export default [
     openSpots: 3,
   },
   {
-    id: 1,
-    title: "Life Lessons with Katie Zaferes",
+    id: 4,
+    title: "Life Lessons with Katie ",
     description:
       'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
     price: 136,
@@ -52,11 +52,11 @@ export default [
       rating: 5.0,
       reviewCount: 6,
     },
-    location: "Nashik",
+    location: "online",
     openSpots: 0,
   },
   {
-    id: 2,
+    id: 5,
     title: "Learn Wedding Photography",
     description:
       "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
@@ -66,11 +66,11 @@ export default [
       rating: 5.0,
       reviewCount: 30,
     },
-    location: "Pimpalgaon",
+    location: "online",
     openSpots: 27,
   },
   {
-    id: 3,
+    id: 6,
     title: "Group Mountain Biking",
     description:
       "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
